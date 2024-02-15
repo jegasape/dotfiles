a0cde81e43d767ba6bd5f6cc60c03c644c52189e
jegasape <jegasape@gmail.com>
2024-02-15 03:32:09 -0400

    [.dotfiles-1707982329354511926]: update-hypr-and-sway-configurations

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -19,6 +19,7 @@ vim.opt.autoindent = true
 
 vim.opt.splitkeep = "cursor"
 vim.opt.mouse = ""
+vim.opt.laststatus = 2
 
 -- automatically install python syntax highlighting
 lvim.builtin.treesitter.ensure_installed = {
